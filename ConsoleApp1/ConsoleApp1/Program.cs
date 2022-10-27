c4f4e7143f52399f664dbabb8584f30e00c011b9
(s) Oscar Thomson <oscar.thomson@students.plymouth.ac.uk>
2022-10-27 10:48:38 +0100

    Added car objects

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,88 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Example");
+            VolkswagenUp myCar = new VolkswagenUp();
+            myCar.status();
+            myCar.refuel(3);
+            myCar.status();
+            myCar.driveMiles(10);
+            myCar.status();
+        }
+    }
+
+    class Car
+    {
+        public string model;
+        public float tankSize; //in gallons
+        public float fuel; //in gallons
+        public float efficiency; //in miles per gallon
+        public float distance; //in miles
+
+        public Car()
+        {
+            model = "Generic";
+            tankSize = 15;
+            fuel = 0;
+            efficiency = 50;
+            distance = 0;
+        }
+        public void playSound()
+        {
+            Console.WriteLine("Vroom");
+        }
+
+        public void status()
+        {
+            Console.WriteLine($"Car status\n----------\nModel: {model}\nFuel: {fuel}/{tankSize} gallons\nFuel Efficiency: {efficiency} mpg\nTotal distance: {distance} miles\n");
+        }
+
+        public void driveMiles(float distance)
+        {
+            float fuelUsed = distance / efficiency;
+            float distanceTraveled;
+
+            if (fuel < fuelUsed)
+            {
+                distanceTraveled = fuel * efficiency;
+                fuelUsed = fuel;
+                fuel = 0;
+            }
+            else
+            {
+                distanceTraveled = distance;
+                fuel -= fuelUsed;
+            }
+
+            distance += distanceTraveled;
+
+            Console.WriteLine($"Tried driving {distance} miles\nSuccessfully drove {distanceTraveled} miles\nUsed {fuelUsed} gallons of fuel\nCurrent fuel is now {fuel} gallons\n");
+        }
+
+        public void refuel(float amount)
+        {
+            float fuelAdded;
+            if (fuel + amount > tankSize)
+            {
+                fuelAdded = tankSize - fuel;
+                fuel = tankSize;
+            }
+            else
+            {
+                fuel += amount;
+                fuelAdded = amount;
+            }
+
+            Console.WriteLine($"Added {fuelAdded} gallons of fuel\nCurrent fuel is now {fuel}/{tankSize}\n");
+        }
+    }
+    class VolkswagenUp : Car
+    {
+        public VolkswagenUp()
+        {
+            model = "VolkswagenUp";
+            efficiency = 55;
+            tankSize = 7.7f;
+
         }
     }
 }
